8efc6e3fdfd2e4798a22ba83b72dbfe3d338b2ee
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2015-04-02 01:34:26 +0900

    only test on the environment .codePointAt is available

--- test.js
+++ test.js
@@ -10,32 +10,38 @@ function runTest(t) {
   t.plan(6);
 
   t.equals(
-    codePoint('0'), 48,
+    codePoint('0'),
+    48,
     'should return a UTF-16-encoded code point of the character.'
   );
 
   t.equals(
-    codePoint('\ud800\udc00'), 65536,
+    codePoint('\ud800\udc00'),
+    65536,
     'should return a code point value of the character which uses a surrogate pair.'
   );
 
   t.equals(
-    codePoint('\udbffa'), 56319,
+    codePoint('\udbffa'),
+    56319,
     'should return a code point value of the string\'s first character.'
   );
 
   t.throws(
-    codePoint.bind(null), /TypeError/,
+    codePoint.bind(null),
+    /TypeError/,
     'should throw a type error when it takes no arguments.'
   );
 
   t.throws(
-    codePoint.bind(null, 1), /TypeError/,
+    codePoint.bind(null, 1),
+    /TypeError/,
     'should throw a type error when the argument is not a string.'
   );
 
   t.throws(
-    codePoint.bind(null, ''), /empty/,
+    codePoint.bind(null, ''),
+    /empty/,
     'should throw an error when the argument is an empty string.'
   );
 }
@@ -45,8 +51,8 @@ test('codePoint()', function(t) {
   runTest(t);
 });
 
-test('codePoint() on ES6+ environment', function(t) {
-  require('string.prototype.codepointat');
+test('codePoint() on ES5 environment', function(t) {
+  String.prototype.codePointAt = undefined;
   codePoint = requireUncached('./');
   runTest(t);
 });
