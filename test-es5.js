f22c132e03d6142f76d973934b959862b7a1380f
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-12-11 08:10:05 +0900

    update devDependencies
    Highlights:
    
    * update ESLint from v3.x to v5.x
    * update @shinnn/eslint-config and follow its rules, for example tab indent
    * introduce Rollup and rollup-config-module to adapt to .mjs-based ecosystem
    * switch from tap to tape for faster instalation
    * introduce nyc-config-common to reduce boilerplates

--- test-es5.js
+++
@@ -1,4 +0,0 @@
-process.env.TEST_ON_ES5 = true;
-delete String.prototype.codePointAt;
-
-require('./test.js');
--- test.js
+++ test.js
@@ -1,58 +1,53 @@
 'use strict';
 
-const requireFromString = require('require-from-string');
-const {rollup} = require('rollup');
-const {test} = require('tap');
+const clearModule = require('clear-module');
+const test = require('tape');
 
-function runTest(description, fn) {
-  test(`${description}${'TEST_ON_ES5' in process.env ? ' on ES5 environment' : ''}`, t => {
-    t.strictEqual(
-      fn('0'),
-      48,
-      description + 'return a UTF-16-encoded code point of the character.'
-    );
+for (const es5 of [false, true]) {
+	test(`codePoint()${es5 ? ' on ES5 environment' : ''}`, t => {
+		if (es5) {
+			delete String.prototype.codePointAt;
+			clearModule('.');
+		}
 
-    t.strictEqual(
-      fn('\ud800\udc00'),
-      65536,
-      'should return a code point of the character which uses a surrogate pair.'
-    );
+		const codePoint = require('.');
 
-    t.strictEqual(
-      fn('\udbffa'),
-      56319,
-      'should return a code point value of the string\'s first character.'
-    );
+		t.equal(
+			codePoint('0'),
+			48,
+			'should return a UTF-16-encoded code point of the character.'
+		);
 
-    t.throws(
-      () => fn(),
-      /undefined is not a string\. Argument must be a string\./,
-      'should throw a type error when it takes no arguments.'
-    );
+		t.equal(
+			codePoint('\ud800\udc00'),
+			65536,
+			'should return a code point of the character which uses a surrogate pair.'
+		);
 
-    t.throws(
-      () => fn(1),
-      /1 is not a string\. Argument must be a string\./,
-      'should throw a type error when the argument is not a string.'
-    );
+		t.equal(
+			codePoint('\udbffa'),
+			56319,
+			'should return a code point value of the string\'s first character.'
+		);
 
-    t.throws(
-      () => fn(''),
-      /empty/,
-      'should throw an error when the argument is an empty string.'
-    );
+		t.throws(
+			() => codePoint(),
+			/undefined is not a string\. Argument must be a string\./u,
+			'should throw a type error when it takes no arguments.'
+		);
 
-    t.end();
-  });
+		t.throws(
+			() => codePoint(1),
+			/1 is not a string\. Argument must be a string\./u,
+			'should throw a type error when the argument is not a string.'
+		);
+
+		t.throws(
+			() => codePoint(''),
+			/empty/u,
+			'should throw an error when the argument is an empty string.'
+		);
+
+		t.end();
+	});
 }
-
-rollup({entry: require('./package.json')['jsnext:main']}).then(bundle => {
-  runTest('require(\'code-point\')', require('.'));
-
-  global.window = {};
-  require('./' + require('./bower.json').main);
-
-  runTest('window.codePoint', global.window.codePoint);
-
-  runTest('Module exports', requireFromString(bundle.generate({format: 'cjs'}).code, 'indedx.jsnext.js'));
-});
